5da8140351ce8477f61e69099ce0a486cd60e651
enormandeau <eric.normandeau.qc@gmail.com>
2014-07-15 11:43:01 -0400

    Added some reporting to mreps_workflow.sh

--- mreps_workflow.sh
+++ mreps_workflow.sh
@@ -11,16 +11,19 @@ BASENAME=$(basename $NAME)
 echo $BASENAME
 
 # Run mreps to extract repeats (**file extension MUST be .fasta**)
+echo "   Running mreps"
 mreps -res 1 -minperiod 2 -maxperiod 6 -minsize 20 -maxsize 1000 -fasta \
     $NAME.fasta > $NAME.mreps_repeats
 
 # Run mreps_preformat.py to prepare the last step of output formating
+echo "   Running mreps_preformat.py"
 mreps_preformat.py $NAME.mreps_repeats | \
     perl -pe 's/([ACTG]) ([ACTG])/\1_\2/g; s/\t/ /g; s/://g; \
     s/\->//; s/ +/\t/g; s/\t+/\t/g; \
     s/([ACTG])_([ACTG])/\1 \2/g' > $NAME.mreps_formatted
 
 # Run mreps_format_light.py to create more usable/readable output
+echo "   Running mreps_format_light.py"
 ./mreps_format_light.py $NAME.mreps_formatted $NAME.fasta $NAME.mreps_final.tsv
 
 # Output the number of markers found and the name of the final output file
